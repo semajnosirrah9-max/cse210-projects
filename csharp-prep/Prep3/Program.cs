e414325e9da3bb861ad26859a5b931338d1ffd1c
James Ratliff <semajnosirrah9@gmail.com>
2026-01-15 17:14:52 -0700

    Random randomGenerator = new Random();         int magicNumber = randomGenerator.Next(1, 101);

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicnumber)
+        {
+            Console.Write("Guess a number between 1-100.\nWhat is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicnumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicnumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
