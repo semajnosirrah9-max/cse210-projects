b0c2cb67b5d5beac989fce2d840c38536858cd83
James Ratliff <semajnosirrah9@gmail.com>
2026-01-15 19:31:16 -0700

    git commit -m "Prep 4 completed"

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers and type 0 when finished.");
+
+        int usernumber = -1;
+
+        while (usernumber != 0)
+        {
+            Console.Write("Enter Number: ");
+            usernumber = int.Parse(Console.ReadLine());
+
+            if (usernumber != 0)
+            {
+                numbers.Add(usernumber);
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int largest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
